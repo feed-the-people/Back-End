dc5cb93241a89d3826bddc0ee93c7e004f804a43
Logan Riffell <lkriffell@gmail.com>
2021-01-04 11:17:00 -0700

    Remove billing_info from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_002907) do
+ActiveRecord::Schema.define(version: 2021_01_04_181526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2020_12_29_002907) do
     t.string "city"
     t.string "state"
     t.string "zip"
-    t.string "billing_info"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
