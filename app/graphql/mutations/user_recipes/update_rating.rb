e0c2fb24dcfff80c5df1cec8a50747afdbeb9d14
Hashim Gari <hashmaster3k@gmail.com>
2021-01-04 16:01:16 -0700

    add call to update Recipe rating

--- app/graphql/mutations/user_recipes/update_rating.rb
+++ app/graphql/mutations/user_recipes/update_rating.rb
@@ -2,21 +2,22 @@ module Mutations
   module UserRecipes
     class UpdateRating < Mutations::BaseMutation
       argument :params, Types::Input::UserRecipes::UpdateType, required: true
-  
+
       field :user_recipe, Types::UserRecipeType, null: false
-  
+
       def resolve(params:)
         rating_params = Hash params
         if valid_rating?(rating_params[:recipe_rating])
-            begin
-                user_recipe = UserRecipe.find(rating_params[:id])
-                user_recipe.update!(recipe_rating: rating_params[:recipe_rating])
-        
-                { user_recipe: user_recipe }
-              rescue ActiveRecord::RecordInvalid => e
-                GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
-                  " #{e.record.errors.full_messages.join(', ')}")
-              end
+          begin
+            user_recipe = UserRecipe.find(rating_params[:id])
+            user_recipe.update!(recipe_rating: rating_params[:recipe_rating])
+            Recipe.update_rating(user_recipe.recipe_id)
+
+            { user_recipe: user_recipe }
+          rescue ActiveRecord::RecordInvalid => e
+            GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+              " #{e.record.errors.full_messages.join(', ')}")
+          end
         else
             GraphQL::ExecutionError.new("Number must be greater than 0 and less than 5.")
         end
