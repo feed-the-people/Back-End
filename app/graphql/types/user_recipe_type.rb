0e24e63c90104b9b2f7a23c22466e005e95116e0
Logan Riffell <lkriffell@gmail.com>
2021-01-05 14:35:53 -0700

    Update user_recipe_type to include recipe

--- app/graphql/types/user_recipe_type.rb
+++ app/graphql/types/user_recipe_type.rb
@@ -1,9 +1,12 @@
 module Types
   class UserRecipeType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: false
-    field :recipe_id, Integer, null: false
+    field :user_id, ID, null: false
+    field :recipe_id, ID, null: false
     field :amounted_donated, Float, null: false
     field :recipe_rating, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :recipe, Types::RecipeType, null: false
   end
 end
