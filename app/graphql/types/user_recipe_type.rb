4022e5d1ce9be7d7ef4996c4e2cf86b3355d9b8e
Logan Riffell <lkriffell@gmail.com>
2021-01-05 14:59:52 -0700

    Fix typo on amountDonated

--- app/graphql/types/user_recipe_type.rb
+++ app/graphql/types/user_recipe_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :user_id, ID, null: false
     field :recipe_id, ID, null: false
-    field :amounted_donated, Float, null: false
+    field :amount_donated, Float, null: false
     field :recipe_rating, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
