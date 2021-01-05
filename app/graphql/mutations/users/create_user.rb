de4563de3a2afd07d3dc67a8bf5f7fb7c2781dca
Corey Carter <crc2231@gmail.com>
2021-01-05 15:30:41 -0700

    Add Users mutations

---
+++ app/graphql/mutations/users/create_user.rb
@@ -0,0 +1,20 @@
+module Mutations
+  module Users
+    class CreateUser < BaseMutation
+      class AuthProviderSignupData < Types::BaseInputObject
+        argument :credentials, Types::AuthProviderCredentialsInput, required: false
+      end
+
+      argument :auth_provider, AuthProviderSignupData, required: true
+
+      type Types::UserType
+
+      def resolve(name: nil, auth_provider: nil)
+        User.create!(
+          username: auth_provider&.[](:credentials)&.[](:username),
+          password: auth_provider&.[](:credentials)&.[](:password)
+        )
+      end
+    end
+  end
+end
