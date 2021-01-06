9176b7119f2285d525a064fa92457c12262a8616
Corey Carter <crc2231@gmail.com>
2021-01-05 17:03:25 -0700

    Add fields to resolver of register_user

--- app/graphql/mutations/users/register_user.rb
+++ app/graphql/mutations/users/register_user.rb
@@ -2,18 +2,33 @@ module Mutations
   module Users
     class RegisterUser < BaseMutation
       class AuthProviderSignupData < Types::BaseInputObject
-        argument :credentials, Types::AuthProviderCredentialsInput, required: false
+        argument :credentials, Types::AuthProviderCredentialsInput, required: true
       end
+
+      argument :first_name, String, required: true
+      argument :last_name, String, required: true
+      argument :street, String, required: true
+      argument :city, String, required: true
+      argument :state, String, required: true
+      argument :zip, String, required: true
+      argument :image, String, required: true
 
       argument :auth_provider, AuthProviderSignupData, required: true
 
       type Types::UserType
 
-      def resolve(name: nil, auth_provider: nil)
+      def resolve(auth_provider: nil, first_name: nil, last_name: nil, street: nil, city: nil, state: nil, zip: nil, image: nil)
         User.create!(
           username: auth_provider&.[](:credentials)&.[](:username),
-          password: auth_provider&.[](:credentials)&.[](:password)
-        )
+          password: auth_provider&.[](:credentials)&.[](:password),
+          first_name: first_name,
+          last_name: last_name,
+          street: street,
+          city: city,
+          state: state,
+          zip: zip,
+          image: image
+                  )
       end
     end
   end
