25cf5931c5de50fc7ffc4ece3909a6f35fe3ec96
Corey Carter <crc2231@gmail.com>
2021-01-05 17:04:00 -0700

    Modify register user spec

--- spec/mutations/users/register_user_spec.rb
+++ spec/mutations/users/register_user_spec.rb
@@ -37,12 +37,12 @@ module Mutations
 
           @user = perform({
             image: 'www.image.com',
-                firstName: 'Test',
-                lastName: 'User',
-                state: 'CO',
-                city: 'Denver',
-                street: '1234 Road Street',
-                zip: '1234'
+            first_name: 'Test',
+            last_name: 'User',
+            state: 'CO',
+            city: 'Denver',
+            street: '1234 Road Street',
+            zip: '1234',
             auth_provider: {
               credentials: {
                 username: 'email@example.com',
@@ -51,7 +51,6 @@ module Mutations
             }
           }
           )
-          binding.pry
           expect(@user.username).to eq('email@example.com')
           expect(User.count).to eq(1)
 
@@ -60,6 +59,12 @@ module Mutations
       json = JSON.parse(response.body)
       expect(json['data']['getUser']['id']).to eq(@user.id.to_s)
       expect(json['data']['getUser']['username']).to eq(@user.username)
+      expect(json['data']['getUser']['firstName']).to eq(@user.first_name)
+      expect(json['data']['getUser']['lastName']).to eq(@user.last_name)
+      expect(json['data']['getUser']['state']).to eq(@user.state)
+      expect(json['data']['getUser']['city']).to eq(@user.city)
+      expect(json['data']['getUser']['zip']).to eq(@user.zip)
+      expect(json['data']['getUser']['image']).to eq(@user.image)
         end
       end
     end
