e558362ba3422cdc319e5284a79d85addcdd0f05
Hashim Gari <hashmaster3k@gmail.com>
2021-01-06 15:53:52 -0700

    add createIngredient mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :register_user, mutation: Mutations::Users::RegisterUser
     field :create_user_recipe, mutation: Mutations::UserRecipes::CreateUserRecipe
     field :update_rating, mutation: Mutations::UserRecipes::UpdateRating
+    field :create_ingredient, mutation: Mutations::Ingredients::CreateIngredient
   end
 end
