bbb9d90e91345c93121fc62c321ce45dd93e17ab
Logan Riffell <lkriffell@gmail.com>
2021-01-06 11:47:01 -0700

    Add userSignIn to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user_recipe, mutation: Mutations::UserRecipes::CreateUserRecipe
     field :update_rating, mutation: Mutations::UserRecipes::UpdateRating
+    field :user_sign_in, mutation: Mutations::Users::UserSignIn
   end
 end
