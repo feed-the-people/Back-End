8a7361863b0d267e45a7d9f96fdb9e635d4f96e0
Cam C <ckc.cameron@gmail.com>
2021-01-06 20:56:40 -0800

    Write happy path test to update user attributes
    * image, username, firstName, lastName, street, city, state, zip

---
+++ spec/requests/mutations/users/update_user_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe 'User Update' do
+  before :each do
+    @user = create(:user, image: "https://robohash.org/my-own-slug.png?size=50x50&set=set1", username: "123user", first_name: "Martha", last_name: "Stewart", street: "123 Westside Ave", city: "Los Angeles", state: "CA", zip: "90210" )
+    @user_id = @user.id
+  end
+
+  describe 'happy-paths' do
+    it 'updates user attributes' do
+      query_string = <<-GRAPHQL
+        mutation {
+          updateUser(input: { params: { id: #{@user_id}, image: "https://robohash.org/new-slug-edit-update.png?size=50x50&set=set1", username: "food_love123", zip: "90272" } }) {
+            user {
+              id
+              image
+              username
+              firstName
+              lastName
+              street
+              city
+              state
+              zip
+            }
+          }
+        }
+      GRAPHQL
+
+      post '/graphql', params: { query: query_string }
+      results = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:data)
+      expect(results[:data]).to be_a(Hash)
+      expect(results[:data]).to have_key(:updateUser)
+      expect(results[:data][:updateUser]).to be_a(Hash)
+      expect(results[:data][:updateUser]).to have_key(:user)
+      expect(results[:data][:updateUser][:user]).to be_a(Hash)
+      expect(results[:data][:updateUser][:user]).to have_key(:id)
+      expect(results[:data][:updateUser][:user][:id]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:image)
+      expect(results[:data][:updateUser][:user][:image]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:username)
+      expect(results[:data][:updateUser][:user][:username]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:firstName)
+      expect(results[:data][:updateUser][:user][:firstName]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:lastName)
+      expect(results[:data][:updateUser][:user][:lastName]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:street)
+      expect(results[:data][:updateUser][:user][:street]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:city)
+      expect(results[:data][:updateUser][:user][:city]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:state)
+      expect(results[:data][:updateUser][:user][:state]).to be_a(String)
+      expect(results[:data][:updateUser][:user]).to have_key(:zip)
+      expect(results[:data][:updateUser][:user][:zip]).to be_a(String)
+    end
+  end
+end
