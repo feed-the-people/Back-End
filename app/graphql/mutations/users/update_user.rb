98d71452d0a474f021e70eb43c7fe04e51d03e36
Cam C <ckc.cameron@gmail.com>
2021-01-07 09:02:01 -0800

    Add id validation method and error handling to UpdateUser mutation

--- app/graphql/mutations/users/update_user.rb
+++ app/graphql/mutations/users/update_user.rb
@@ -7,30 +7,21 @@ module Mutations
 
       def resolve(params:)
         user_params = Hash params
-        if valid_id?(user_params[:id])
+        if valid_id?(user_params[:id]) &&
           user = User.find(user_params[:id])
           user.update!(user_params)
 
           { user: user }
+        else
+          if !valid_id?(user_params[:id])
+            GraphQL::ExecutionError.new("No record of User with ID #{user_params[:id]}")
+          end
         end
       end
 
       def valid_id?(id)
         User.find(id) rescue false
       end
-
-      # def valid_user_params?(user_params)
-      #   if !valid_username?(user_params[:username]) || !valid_first_name?(user_params[:firstName]) ||
-      #   !valid_last_name?(user_params[:lastName]) || !valid_street?(user_params[:street]) || !valid_city?(user_params[:city]) || !valid_state?(user_params[:state]) || !valid_zip?(user_params[:zip])
-      #     false
-      #   else
-      #     true
-      #   end
-      # end
-      #
-      # def valid_username?(username)
-      #
-      # end
     end
   end
 end
