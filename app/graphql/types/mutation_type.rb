1a614ea71650cc33095b9fbd5a654dca695fa632
Cam C <ckc.cameron@gmail.com>
2021-01-06 20:59:55 -0800

    Add UpdateUser mutation to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :update_user, mutation: Mutations::Users::UpdateUser
     field :update_rating, mutation: Mutations::UserRecipes::UpdateRating
   end
 end
