085bd744827a921ede5aed897315944336d2ab7e
Cam C <ckc.cameron@gmail.com>
2021-01-07 15:21:59 -0800

    Remove unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,4 @@ class User < ApplicationRecord
 
   validates :password_digest, presence: true
   validates :username, uniqueness: true, presence: true
-  # validates_format_of :zip,
-  #                     :with => %r{\d{5}(-\d{4})?},
-  #                     :message => "should be formatted as 12345 or 12345-1234"
 end
