c0215c1f7776426c618ee187e46d539cd34dc76b
Hashim Gari <hashmaster3k@gmail.com>
2021-01-08 11:38:37 -0700

    change charity_id to string due to octal error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_021328) do
+ActiveRecord::Schema.define(version: 2021_01_08_183254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_021328) do
     t.string "title"
     t.text "description"
     t.string "instructions"
-    t.integer "charity_id"
+    t.string "charity_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
