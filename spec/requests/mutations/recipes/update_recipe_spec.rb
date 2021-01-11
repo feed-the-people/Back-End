643b17c3fc3a7630458336f646995cddf4aa5601
Cam C <ckc.cameron@gmail.com>
2021-01-10 18:22:41 -0800

    Write sad path test for invalid recipe id input

--- spec/requests/mutations/recipes/update_recipe_spec.rb
+++ spec/requests/mutations/recipes/update_recipe_spec.rb
@@ -10,15 +10,49 @@ RSpec.describe 'Update Recipe' do
     it 'updates recipe attributes' do
       mutation_string = <<-GRAPHQL
         mutation {
-          updateRecipe(input: { params: {
-            userId: #{@user.id}
-            title: "Spaghetti"
-            description: "il classico è molto delizioso"
-            instructions: "1. Noodles! 2. Italian Sausage and Veggies! 3. Basil Sauce! 4. Yum!"
-            ingredients: [{ name: "Spaghetti Noodles" amount: "1 lb" }, { name: "Italian Sausage" amount: "2 lbs" }, { name: "Basil Sauce" amount: "2 cans" }]
-          }) {
+          updateRecipe(input: {params: {
+            id: #{@recipe.id},
+            userId: #{@user.id},
+            title: "Spaghetti",
+            description: "il classico è molto delizioso",
+            instructions: "1. Noodles! 2. Italian Sausage and Veggies! 3. Basil Sauce! 4. Yum!",
+            ingredients: [{ name: "Spaghetti Noodles", amount: "1 lb"}, {name: "Italian Sausage", amount: "2 lbs"}, {name: "Basil Sauce", amount: "2 cans"}]}}) {
+              recipe {
+                id
+                title
+                description
+                instructions
+                ingredients {
+                  name
+                  amount
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      post '/graphql', params: { query: mutation_string }
+      results = JSON.parse(response.body, symbolize_names: true)
+      # binding.pry
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:data)
+      expect(results[:data]).to be_a(Hash)
+      expect(results[:data]).to have_key(:updateRecipe)
+      expect(results[:data][:updateRecipe]).to be_a(Hash)
+      expect(results[:data][:updateRecipe]).to have_key(:recipe)
+    end
+  end
+
+  describe 'sad-paths' do
+    it 'cannot update a user with non-existing id' do
+      query_string = <<-GRAPHQL
+        mutation {
+          updateRecipe(input: {params: {id: 15, userId: 1, title: "Lasagna", description: "Layers of goodness!"}}) {
             recipe {
               id
+              userId
+              image
               title
               description
               instructions
@@ -31,15 +65,13 @@ RSpec.describe 'Update Recipe' do
         }
       GRAPHQL
 
-      post '/graphql', params: { query: mutation_string }
+      post '/graphql', params: { query: query_string }
       results = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
+
       expect(results).to be_a(Hash)
-      expect(results).to have_key(:data)
-      expect(results[:data]).to be_a(Hash)
-      expect(results[:data]).to have_key(:updateRecipe)
-      expect(results[:data][:updateRecipe]).to be_a(Hash)
-      expect(results[:data][:updateRecipe]).to have_key(:recipe)
+      expect(results).to have_key(:errors)
+      expect(results[:errors]).to be_an(Array)
+      expect(results[:errors][0][:message]).to eq("No record of Recipe with ID 15")
     end
   end
 end
